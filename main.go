5025815ec0f55213816b8190a9cae0c3b9e3faba
machangxin <machangxinq@gmail.com>
2022-03-18 08:13:58 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/robfig/cron/v3"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var barkKey string
+
+func main() {
+	barkKey = "test"
+	var wg sync.WaitGroup
+	wg.Add(1)
+	nyc, _ := time.LoadLocation("Asia/Shanghai")
+	saveLog(barkPush("测试发送", barkKey, "测试"))
+	c := cron.New(cron.WithLocation(nyc))
+	_, err := c.AddFunc("0 7-23 * * MON-FRI", getlubu)
+	if err != nil {
+		saveLog(err.Error())
+		return
+	}
+	getlubu()
+	c.Start()
+	wg.Wait()
+}
+
+func getlubu() {
+	// Request the HTML page.
+	res, err := http.Get("https://www.boc.cn/sourcedb/whpj/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(res.Body)
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gong := "body > div.wrapper > div.BOC_main > div.publish > div:nth-child(3) > table > tbody > tr:nth-child("
+	namegps := ") > td:nth-child(1)"
+	salegps := ") > td:nth-child(2)"
+	buygps := ") > td:nth-child(4)"
+	timegps := ") > td:nth-child(8)"
+	for i := 2; i < 30; i++ {
+		name := gong + strconv.Itoa(i) + namegps
+		nameText := doc.Find(name).Text()
+		if nameText == "卢布" {
+			saleName := gong + strconv.Itoa(1) + ") > th:nth-child(2)"
+			buyName := gong + strconv.Itoa(1) + ") > th:nth-child(4)"
+			timeName := gong + strconv.Itoa(1) + ") > th:nth-child(8)"
+			saleDom := gong + strconv.Itoa(i) + salegps
+			buyDom := gong + strconv.Itoa(i) + buygps
+			timeDom := gong + strconv.Itoa(i) + timegps
+			saleNameText := doc.Find(saleName).Text()
+			buyNameText := doc.Find(buyName).Text()
+			timeNameText := doc.Find(timeName).Text()
+			saleText := doc.Find(saleDom).Text()
+			buyText := doc.Find(buyDom).Text()
+			timeText := doc.Find(timeDom).Text()
+			body := saleNameText + ": " + saleText + "\n" + buyNameText + ": " + buyText + "\n" + timeNameText + ": " + timeText
+			saveLog(barkPush(body, barkKey, nameText))
+			break
+		}
+		if i == 29 {
+			result, _ := ioutil.ReadAll(res.Body)
+			saveLog(string(result))
+			saveLog(barkPush("貌似被封了", barkKey, nameText))
+
+		}
+	}
+}
---
+++ util.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func barkPush(body, key, title string) string {
+	mapdata := make(map[string]string)
+	mapdata["body"] = body
+	mapdata["device_key"] = key
+	mapdata["title"] = title
+	b, _ := json.Marshal(mapdata)
+	// 超时时间：20秒
+	client := &http.Client{Timeout: 20 * time.Second}
+	pushurl := "https://api.day.app/push"
+	header := "application/json; charset=utf-8"
+	resp, err := client.Post(pushurl, header, bytes.NewBuffer(b))
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+	result, _ := ioutil.ReadAll(resp.Body)
+	return string(result) + "\n"
+}
+
+func FileExist(path string) bool {
+	_, err := os.Lstat(path)
+	return !os.IsNotExist(err)
+}
+
+func saveLog(mess string) {
+	filePath := "./send.log"
+	exist := FileExist(filePath)
+	if !exist {
+		fileCreate, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("文件创建失败", err)
+		}
+		err = fileCreate.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("文件打开失败", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+	//写入文件时，使用带缓存的 *Writer
+	write := bufio.NewWriter(file)
+	write.WriteString(mess)
+	//Flush将缓存的文件真正写入到文件中
+	write.Flush()
+}
